12353ecd84b91f7648a628765fd3cdbac366d510
GreenGray5046 <100970870+GreenGray5046@users.noreply.github.com>
2025-08-27 17:27:58 -0400

    Create example.py
    This one makes an animation of the riemann zeta function (takes forever to load)

---
+++ example.py
@@ -0,0 +1,30 @@
+import numpy as np
+import morphing_grid as mg
+import mpmath as mp
+import scipy as scp
+import scipy.special as scps
+
+def zeta(z):
+    return np.array([complex(mp.zeta(complex(val))) for val in np.ravel(z)]).reshape(z.shape)
+    #return np.sin(z)
+
+# Define the LaTeX title for your  function
+custom_latex_title = "riemann zeta"
+
+# Create an instance of the ConformalAnimator class with your function
+animator = mg.ConformalAnimator(
+    f=zeta,
+    latex_title=custom_latex_title,
+    domain=(-2, 2, -2, 2),
+    grid_steps=25,
+    n_frames=200
+)
+
+# Run the animation and save it to a file
+#animator.animate_grid(save_path="custom_animation.gif", fps=40)
+
+# Show a preview
+animator.animate_grid(show=True, fps=30)
+
+# You can also call the demo function
+# demo()
