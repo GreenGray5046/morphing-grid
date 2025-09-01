fae010731310ed017d6bbfd679df31790a9056b8
GreenGray5046 <100970870+GreenGray5046@users.noreply.github.com>
2025-09-01 17:05:32 -0400

    Update example_complex.py

--- examples/example_complex.py
+++ examples/example_complex.py
@@ -1,33 +1,26 @@
 import math
 import numpy as np
 import morphing_grid as mg
-import sympy as sp
-
-z = sp.symbols('z')
+from scipy.special import gamma
 
 def zeta(z_val):
-    # This is your vectorized function. It will be passed to f.
-    # Note: `z_val` is a NumPy array of complex numbers.
-    return np.sin(z_val)
+    # This is your function. It will be passed to f.
+    return (z_val**2)-1
 
-# Define the symbolic SymPy expression for your custom function.
-# This is required for the new derivative-based refinement.
-custom_sympy_expr = sp.sin(z)
-
-# Define the LaTeX title for your custom function
-custom_latex_title = "$f(z) = sin(z) $"
+# Define the title for your custom function (supports LaTeX)
+custom_latex_title = "$ f(z) = z^2 - 1 $"
 
 # Create an instance of the ConformalAnimator class with your function
 animator = mg.ConformalAnimator(
     f=zeta,
-    sympy_f_expr=custom_sympy_expr, # Pass the new symbolic expression here
     latex_title=custom_latex_title,
-    domain=(-4, 4, -4, 4),
+    domain=(-2, 2, -2, 2),
     grid_steps=25,
     n_frames=200,
     line_resolution=200
 )
-
-# Run the animation and save it to a file
+# Show preview
 animator.animate_grid(show=True, fps=30)
+
+# Save file
 # animator.animate_grid(save_path="custom_animation.gif", fps=40)
